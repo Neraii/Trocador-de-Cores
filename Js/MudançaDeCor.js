9e664b6c8b3492869a38ca21e8555c39a8ef3d8a
Cairo Henrique <cairothomaz@gmail.com>
2023-03-07 18:23:24 -0300

    Programa Terminado

---
+++ Js/MudançaDeCor.js
@@ -0,0 +1,23 @@
+function mudandoDeCor() {
+    //Pega a cor e faz o fundo mudar de cor
+    let sequencia = pegaSequenciaAleatoria();
+    document.body.style.backgroundColor = "#" + sequencia;
+
+}
+
+function pegaSequenciaAleatoria() {
+    //feito para fazer uma sequencia hexadecimal
+    let array = '';
+    for (let i = 0; i < 8; i++) {
+        
+        numero = Math.round(Math.random() * 16);
+        array += numero.toString(16);
+    }
+    return array
+}
+
+let botao = document.querySelector("#botao");
+botao.addEventListener("click", mudandoDeCor);
+
+mudandoDeCor();
+
